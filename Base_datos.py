a1647f7f7284837b38a55860df31a7a52df9e5b6
nelsonrinconc86 <48596015+nelsonrinconc86@users.noreply.github.com>
2025-05-08 11:07:31 -0500

    Create Base_datos.py

---
+++ Base_datos.py
@@ -0,0 +1,14 @@
+class Base_datos:
+  def __init__(self):
+    self.lista_datos = [["P001", "Pequeña","15dias"], ["P002", "Grande","5dias"]]
+
+  def imprimir_info(self):
+    for self.lista_datos in range(self.lista_datos.len):
+      for(info_pollo in self.lista_datos)
+        print(info_pollo)
+
+
+  def crear_pollo
+  def leer_pollo
+  def actualizar_pollo
+  def eliminar_pollo
